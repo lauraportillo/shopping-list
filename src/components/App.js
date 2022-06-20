f0eb98aac595f1366cc288d5695b5f487bb6271f
Laura Portillo <lauraportillo.ark@gmail.com>
2022-06-20 13:25:39 +0200

    add header main and footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 // React
 import React, { useState } from 'react';
+import Header from './Header';
+import List from './List';
+import Footer from './Footer';
 import '../stylesheets/App.scss';
+import '../stylesheets/Reset.scss';
+
 
 function App() {
-  return <div></div>;
+  return <>
+    <Header />
+    <List />
+    <Footer />
+  </>;
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <>
+            <h1>shopping</h1>
+            <h2>don't forget anything</h2>
+        </>
+    )
+}
+
+export default Header;
