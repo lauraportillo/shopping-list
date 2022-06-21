eacb414aa9192075b51fe0d6716bc813cc8bd1c2
Laura Portillo <lauraportillo.ark@gmail.com>
2022-06-21 12:55:22 +0200

    add an input to add items

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
 // React
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
+// Components
 import Header from './Header';
+import InputItem from './InputItem';
 import List from './List';
 import Footer from './Footer';
+// Styles
 import '../stylesheets/App.scss';
 import '../stylesheets/Reset.scss';
 
 
+
 function App() {
+
+  const [items, setItems] = useState([
+    { itemName: 'item 1', quantity: 1, isSelected: false },
+    { itemName: 'item 2', quantity: 3, isSelected: true },
+    { itemName: 'item 3', quantity: 2, isSelected: false },
+  ]);
+
   return <div className="container">
     <Header />
+    <InputItem />
     <List />
     <Footer />
   </div>;
---
+++ src/components/InputItem.js
@@ -0,0 +1,20 @@
+// React
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+// Styles
+import '../stylesheets/InputItem.scss';
+
+function InputItem() {
+    return (
+        <div className='containerInputItem'>
+            <input className='add-item-input' placeholder='Add an item...' />
+            <FontAwesomeIcon icon={faPlus} />
+
+            {/* <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className='add-item-input' placeholder='Add an item...' />
+            <FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} /> */}
+        </div>
+    )
+}
+
+export default InputItem;
