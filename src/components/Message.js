c831345f20b443ef54d3c2c7f530b85fe08ca057
Laura Portillo <lauraportillo.ark@gmail.com>
2022-07-04 12:46:27 +0200

    add styles to message

--- src/components/Message.js
+++ src/components/Message.js
@@ -6,7 +6,8 @@ import '../stylesheets/Message.scss';
 function Message() {
     return (
         <div className="containerMessage">
-            <p className="containerMessage__text">write down your shopping list</p>
+            <p className="containerMessage__title"><span className="containerMessage__bold">no item</span> in the list</p>
+            <p className="containerMessage__subtitle">write down your <span className="containerMessage__bold">shopping</span></p>
             <img src={fruitImage} alt="fruit" className="containerMessage__img" />
         </div>
     )
