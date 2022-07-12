4dd16dae906f742af7a7d3487c195b0e935526a4
Laura Portillo <lauraportillo.ark@gmail.com>
2022-07-12 12:01:54 +0200

    optimize code using watermelons component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,12 @@
 // Components
-import Watermelon1 from './Watermelon1';
-import Watermelon2 from './Watermelon2';
-import Watermelon3 from './Watermelon3';
+import Watermelons from './Watermelons';
 // Styles
 import '../stylesheets/Header.scss';
 
 function Header() {
     return (
         <header className="containerHeader">
-            <section className="containerHeader__watermelons">
-                <Watermelon1 />
-                <Watermelon2 />
-                <Watermelon3 />
-            </section>
-
+            <Watermelons />
             <section className="containerHeader__title">
                 <h1 className="containerHeader__title--title">shopping</h1>
                 <h2 className="containerHeader__title--subtitle">and eat healthy!</h2>
---
+++ src/components/Watermelons.js
@@ -0,0 +1,51 @@
+// Styles
+import '../stylesheets/Watermelons.scss';
+
+function Watermelons() {
+    return (
+
+        <section className="containerWatermelons">
+
+            <div className="watermelon1">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+            </div>
+
+            <div className="watermelon2">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+            </div>
+
+            <div className="watermelon3">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+            </div>
+
+
+        </section>
+
+
+
+
+    )
+}
+
+export default Watermelons;
