0a1e7bb87afd81f9602013851f1b3ec9b09e9fed
Laura Portillo <lauraportillo.ark@gmail.com>
2022-07-14 10:24:30 +0200

    update date

--- src/components/CurrentDate.js
+++ src/components/CurrentDate.js
@@ -1,7 +1,6 @@
 const CurrentDate = () => {
   let showDate = new Date();
-  // let displayTodayDate = ('0' + showDate.getDate()).slice(-2) + '/' + ('0' + (showDate.getMonth() + 1)).slice(-2) + '/' + showDate.getFullYear();
-  let displayTodayDate = (showDate.getFullYear() + '/' + ('0' + (showDate.getMonth() + 1)).slice(-2) + '/' + ('0' + showDate.getDate()).slice(-2));
+  let displayTodayDate = ('0' + showDate.getDate()).slice(-2) + '/' + ('0' + (showDate.getMonth() + 1)).slice(-2) + '/' + showDate.getFullYear();
 
   return (
     <div className="date">
