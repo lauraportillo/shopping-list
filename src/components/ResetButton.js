2de6278b5a0152736e788d2fc880cd0e8ed25eb8
Laura Portillo <lauraportillo.ark@gmail.com>
2022-07-19 15:58:26 +0200

    implement sweetalert2

--- src/components/ResetButton.js
+++ src/components/ResetButton.js
@@ -1,17 +1,42 @@
 // React
 import React from 'react';
+import Swal from 'sweetalert2'
 import { IconTrash } from '@tabler/icons';
 // Styles
 import '../stylesheets/ResetButton.scss';
 
 const ResetButton = (props) => {
+
   const handleReset = () => {
     props.handleReset();
   };
+  const showAlert = () => {
+    Swal.fire({
+      title: 'Delete list',
+      text: 'Sure about this?',
+      icon: 'warning',
+      showDenyButton: true,
+      denyButtonText: 'No',
+      denyButtonColor: '#3085d6',
+      confirmButtonText: 'Yes',
+      confirmButtonColor: '#3085d6'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        handleReset();
+        Swal.fire({
+          text: 'Shoping list deleted',
+          icon: 'success'
+        })
+      }
+    })
+
+  }
+
+
 
   return (
     <>
-      <button className="resetBtn" onClick={handleReset} title='delete list'>
+      <button className="resetBtn" onClick={() => showAlert()} title='delete list'>
         <IconTrash color="#df4a64" size={20} stroke={1.5} />
       </button>
     </>
