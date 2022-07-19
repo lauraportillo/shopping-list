f323f419cea15831059b6ec1919bd51daeb6914d
Laura Portillo <lauraportillo.ark@gmail.com>
2022-07-19 14:39:36 +0200

    remove sweet alert as it has vulnerabilities

--- src/components/ResetButton.js
+++ src/components/ResetButton.js
@@ -1,6 +1,6 @@
 // React
 import React from 'react';
-import swal from 'sweetalert';
+// import swal from 'sweetalert';
 import { IconTrash } from '@tabler/icons';
 // Styles
 import '../stylesheets/ResetButton.scss';
@@ -10,26 +10,26 @@ const ResetButton = (props) => {
     props.handleReset();
   };
 
-  const showAlert = () => {
-    swal({
-      title: 'Delete list',
-      text: 'Sure about this?',
-      icon: 'warning',
-      buttons: ['No', 'Yes']
-    }).then(response => {
-      if (response) {
-        handleReset();
-        swal({
-          text: 'Shoping list deleted',
-          icon: 'success'
-        })
-      }
-    })
-  }
+  // const showAlert = () => {
+  //   swal({
+  //     title: 'Delete list',
+  //     text: 'Sure about this?',
+  //     icon: 'warning',
+  //     buttons: ['No', 'Yes']
+  //   }).then(response => {
+  //     if (response) {
+  //       handleReset();
+  //       swal({
+  //         text: 'Shoping list deleted',
+  //         icon: 'success'
+  //       })
+  //     }
+  //   })
+  // }
 
   return (
     <>
-      <button className="resetBtn" onClick={() => showAlert()} title='delete list'>
+      <button className="resetBtn" onClick={handleReset} title='delete list'>
         <IconTrash color="#df4a64" size={20} stroke={1.5} />
       </button>
     </>
